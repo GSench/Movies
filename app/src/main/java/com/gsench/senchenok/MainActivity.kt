335b68060125733206a8f39c5f4c6eb8897efc1a
GSench <grishasenchenok@yandex.ru>
2024-02-09 23:15:14 +0300

    ViewBinding; Toolbar;

--- app/src/main/java/com/gsench/senchenok/MainActivity.kt
+++ app/src/main/java/com/gsench/senchenok/MainActivity.kt
@@ -2,10 +2,18 @@ package com.gsench.senchenok
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.gsench.senchenok.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.mainToolbar.title = getString(R.string.popular_title)
+        setSupportActionBar(binding.mainToolbar)
     }
+
 }
